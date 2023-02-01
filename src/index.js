e0b4af1b80524aac0df33dea51b4e145f79bcee9
Bastian Friedrich <bastian.friedrich@cheers.io>
2023-02-01 18:33:17 +0100

    install puppeteer, create test route to check if puppeteer is working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
-const express = require( "express" );
+const express = require("express");
+const edgeChromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
+
 const app = express();
 
-// define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+//  Define a route handler for the default home page
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+});
+
+//  Test chrominum handler
+const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+
+app.get("/test", async (req, res) => {
+    //  Open chrome
+    const executablePath = await edgeChromium.executablePath || LOCAL_CHROME_EXECUTABLE
+    
+    const browser = await puppeteer.launch({
+        executablePath,
+        args: edgeChromium.args,
+        headless: true,
+    });
+
+    const page = await browser.newPage();
+
+    //  Go to test page
+    await page.goto('https://www.google.com');
+    const title = await page.title();
+    
+    await browser.close()
+
+    res.send(title);
+});
 
 module.exports = app;
