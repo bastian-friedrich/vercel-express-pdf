7ee1d009d53ea9d1cd8c1693f689ae044c8a844b
Bastian Friedrich <bastian.friedrich@cheers.io>
2023-02-01 18:43:44 +0100

    create pdf

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const edgeChromium = require("chrome-aws-lambda");
 const puppeteer = require("puppeteer-core");
+const fs = require('fs');
 
 const app = express();
 
@@ -31,6 +32,42 @@ app.get("/test", async (req, res) => {
     await browser.close()
 
     res.send(title);
+});
+
+//  Create pdf
+app.get("/pdf", async (req, res) => {
+    //  Read template
+    const html = await fs.readFileSync('./src/template.html', { encoding:'utf8', flag:'r' });
+
+    //  Open chrome
+    const executablePath = await edgeChromium.executablePath || LOCAL_CHROME_EXECUTABLE
+    
+    const browser = await puppeteer.launch({
+        executablePath,
+        args: edgeChromium.args,
+        headless: true,
+    });
+
+    const page = await browser.newPage();
+
+    //  Add content
+    await page.setContent(html, { waitUntil: 'networkidle0' });
+
+    //  Set screen instead of print css
+    await page.emulateMediaType('screen');
+
+    //  Create pdf
+    const pdf = await page.pdf({
+        margin: { top: '2.5cm', right: '2.54cm', bottom: '2.54cm', left: '2.54cm' },
+        format: 'a4',
+        printBackground: true
+    })
+    
+    await browser.close()
+
+    res.setHeader('Content-Disposition', 'attachment; filename=file.pdf');
+    res.setHeader('Content-type', 'application/pdf')
+    res.send(pdf);
 });
 
 module.exports = app;
